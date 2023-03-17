00c525cb8ed9b9aa49bd1deeb264cfd799b73e28
MicheleRai <rainonemichele1999@gmail.com>
2023-03-17 14:54:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿int[] numbers = { 2, 6, 7, 5, 3, 9 };
+
+Console.WriteLine("Array originale: ");
+
+printArray(numbers);
+
+Console.WriteLine("Array al quadrato: ");
+
+printArray(ElevaArrayAlQuadrato(numbers));
+
+Console.WriteLine("Array originale: ");
+
+printArray(numbers);
+
+void printArray(int[] array)
+{
+    Console.WriteLine(arrayToString(array));
+}
+
+string arrayToString(int[] array)
+{
+    var result = "[";
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += array[i].ToString();
+
+        if (i < array.Length - 1)
+        {
+            result += ", ";
+        }
+    }
+
+    result += "]";
+
+    return result;
+}
+
+int Quadrato(int num)
+{
+    var result = num * num;
+
+    return result;
+}
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] quadratoArray = new int[array.Length];
+    for (int i = 0;i < array.Length; i++)
+    {
+        quadratoArray[i] = Quadrato(array[i]);
+    }
+
+    return quadratoArray;
+}
+
+int sommaElementiArray(int[] array)
+{
+
+}
